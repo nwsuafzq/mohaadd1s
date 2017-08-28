fa9a2a51551aa0749dba0bed7a605d72431ea5be
nwsuafzq <804194244@qq.com>
2017-08-28 18:14:27 +0800

    add1s

--- test.sh
+++ test.sh
@@ -3,13 +3,12 @@ let i=0
 while true
 do
 let i++
-echo>>./README.ms
+echo>>./README.md
 echo $i >>./README.md 
 date -u '+%Y.%m.%d +1s'>>./README.md
 echo >>  ./README.md
 git add .
 git commit -m 'add1s'
-git push origin
- 
+git push origin 
 sleep 86400
 done
